f5c4425b8ba3ddb57edf889bc3331c52e54f355c
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-08-31 15:45:01 -0700

    refactor the do while loop with a for loop.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const main = document.querySelector('main');
 const randomNumber = getRandomNumber(10);
-let guess;
 let message;
 
 //Keep track of guess attemps
@@ -12,18 +11,17 @@ function getRandomNumber(upper) {
 
 // TODO: Use a loop to create a number guessing game
 
-
-do {
- guess = prompt("Please guess a number from 1 to 10.");
+for (let i = 1; i <= 10; i++ ) {
+ let guess = prompt("Please guess a number from 1 to 10.");
   attempts++;
   // Terimate loop if the user guesses the number
   if ( parseInt(guess) === randomNumber) {
-    message = `It took you ${attempts} tries to guess the number ${randomNumber}.`
+    message = `It took you ${i} tries to guess the number ${randomNumber}.`
     break;
   } else {
     message = `You did not guess the number. The number was ${randomNumber}.`;
   }
-} while ( attempts < 10 );
+}
 
 main.innerHTML = `<h1>${message}</h1>`;
 
