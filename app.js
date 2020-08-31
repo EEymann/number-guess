12aa811ab46b8cf93705fa355e8900cb326b828d
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-08-31 15:38:26 -0700

    prompt the user to guess a number from one to ten, using a do while loop to iterate through a random number generator.

---
+++ app.js
@@ -0,0 +1,34 @@
+const main = document.querySelector('main');
+const randomNumber = getRandomNumber(10);
+let guess;
+let message;
+
+//Keep track of guess attemps
+let attempts = 0;
+
+function getRandomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1;
+}
+
+// TODO: Use a loop to create a number guessing game
+
+
+do {
+ guess = prompt("Please guess a number from 1 to 10.");
+  attempts++;
+  // Terimate loop if the user guesses the number
+  if ( parseInt(guess) === randomNumber) {
+    message = `It took you ${attempts} tries to guess the number ${randomNumber}.`
+    break;
+  } else {
+    message = `You did not guess the number. The number was ${randomNumber}.`;
+  }
+} while ( attempts < 10 );
+
+main.innerHTML = `<h1>${message}</h1>`;
+
+
+
+//  1) Ask the user to enter a number and assign that value to the `guess` variable
+//  2) End the loop when the user's guess matches the random number
+//  3) Display a message letting the user know they guessed the number
